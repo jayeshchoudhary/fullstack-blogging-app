5d4b2e2f4a431b049907daacfde6e5dbda518ab8
Jayesh Choudhary <jayeshchoudhary619@gmail.com>
2022-11-26 17:25:51 +0530

    final changes

--- backend/index.js
+++ backend/index.js
@@ -7,24 +7,17 @@ const app = express();
 const cors = require("cors");
 const PORT = 8001;
 
-console.log(process.env.MONGODB_URI);
-console.log(process.env.NODE_ENV);
-
 app.use(express.json());
 app.use(cors());
 connectDb();
 
 app.use("/api/blog/", blogRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-console.log("test");
-//*Set static folder up in production
-// const __dirname = path.resolve();
+// Set static folder up in production
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
 );
-// }
 
 app.listen(PORT, () => {
     console.log("Backend is running on port:", PORT);
